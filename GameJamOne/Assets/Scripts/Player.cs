c7da24478fca440f8faed7f724a88bb4442ff0fd
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-05-02 22:33:18 -0400

    Player Duck, Player Stop moving too far left or right

--- GameJamOne/Assets/Scripts/Player.cs
+++ GameJamOne/Assets/Scripts/Player.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
+    [SerializeField] private float playerJumpHeight;
+    [SerializeField] private Rigidbody playerRiBo;
 
     private Vector3 currentPosition;
     private Vector3 initialPosition;
+    private Vector3 initialScale;
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.position;
         currentPosition = transform.position;
+        initialScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -21,12 +25,12 @@ public class Player : MonoBehaviour
     {
         transform.position += transform.forward * Time.deltaTime * playerSpeed;
 
-        if (Input.GetKeyDown(KeyCode.D)) 
+        if (Input.GetKeyDown(KeyCode.D) && currentPosition.x < 5) 
         {
             transform.position = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z);
             currentPosition = transform.position;
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && currentPosition.x > -5)
         {
             transform.position = new Vector3(transform.position.x - 5, transform.position.y, transform.position.z);
             currentPosition = transform.position;
@@ -36,5 +40,21 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             currentPosition = initialPosition;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRiBo.AddRelativeForce(new Vector3(transform.position.x, transform.position.y * playerJumpHeight, transform.position.z));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y /2, transform.localScale.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            currentPosition = transform.position;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2, transform.localScale.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        }
+        
     }
 }
