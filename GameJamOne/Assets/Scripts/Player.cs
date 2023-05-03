c6c03e9541a265a6b0190eb216a49415ed81d7ab
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-05-03 10:37:17 -0400

    Setup reset timer after hitting an obsticle

--- GameJamOne/Assets/Scripts/Player.cs
+++ GameJamOne/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,12 +10,15 @@ public class Player : MonoBehaviour
     [SerializeField] private float playerJumpHeight;
     [SerializeField] private Rigidbody playerRiBo;
 
+    private float resetTimer;
     private Vector3 currentPosition;
     private Vector3 initialPosition;
     private Vector3 initialScale;
+    private float initialSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        initialSpeed = playerSpeed;
         initialPosition = transform.position;
         currentPosition = transform.position;
         initialScale = transform.localScale;
@@ -23,14 +27,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        resetTimer += Time.deltaTime;
+        Debug.Log(resetTimer);
         transform.position += transform.forward * Time.deltaTime * playerSpeed;
+        MovementInput();
+        CountDownStart();
+    }
 
-        if (Input.GetKeyDown(KeyCode.D) && currentPosition.x < 5) 
+    private void OnTriggerEnter(Collider other)
+    {
+        playerSpeed = 0;
+        transform.position = initialPosition;
+        ResetTimer();
+    }
+    private void MovementInput()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && currentPosition.x < 4)
         {
             transform.position = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z);
             currentPosition = transform.position;
         }
-        if (Input.GetKeyDown(KeyCode.A) && currentPosition.x > -5)
+        if (Input.GetKeyDown(KeyCode.A) && currentPosition.x > -4)
         {
             transform.position = new Vector3(transform.position.x - 5, transform.position.y, transform.position.z);
             currentPosition = transform.position;
@@ -42,11 +59,15 @@ public class Player : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRiBo.AddRelativeForce(new Vector3(transform.position.x, transform.position.y * playerJumpHeight, transform.position.z));
+            playerRiBo.AddForce(new Vector3(transform.position.x, transform.position.y * playerJumpHeight, transform.position.z));
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            playerRiBo.AddForce(new Vector3(transform.position.x, transform.position.y * (-playerJumpHeight / 3), transform.position.z));
         }
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y /2, transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 2, transform.localScale.z);
             transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
             currentPosition = transform.position;
         }
@@ -55,6 +76,21 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2, transform.localScale.z);
             transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
         }
-        
+    }
+
+    private void ResetTimer()
+    {
+        resetTimer = 0;
+        Debug.Log("Time was Reset to 0");
+    }
+
+    private void CountDownStart()
+    {
+        if (resetTimer > 3 && transform.position == initialPosition)
+        {
+            Debug.Log("Should Start Moving Now.");
+            playerSpeed = initialSpeed;
+            resetTimer = 0;
+        }
     }
 }
