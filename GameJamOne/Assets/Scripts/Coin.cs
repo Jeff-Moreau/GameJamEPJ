0071f26001d6270ae1d8e033a463c60da2656ccb
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-05-04 20:24:01 -0400

    Coin destroy when off camera

--- GameJamOne/Assets/Scripts/Coin.cs
+++ GameJamOne/Assets/Scripts/Coin.cs
@@ -8,22 +8,18 @@ public class Coin : MonoBehaviour
     public float rotationSpeed;
     public Vector3 rotationDirection;
 
-    GameObject testPlayer;
-    float checkZPos;
     private void Start()
     {
-        /*testPlayer = gameObject.
-        checkZPos = testPlayer.transform.position.z;
-        checkZPos += 5;*/
+
     }
     private void Update()
     {
         transform.Rotate(rotationSpeed * rotationDirection * Time.deltaTime);
-        /*if (checkZPos > transform.position.z)
-        {
-            Destroy(gameObject);
-        }*/
+
     }
 
-    
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
 }
