3bf7f25cdffb7d253a5b702fbb99f00a56969049
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-05-06 00:22:41 -0400

    last

--- GameJamOne/Assets/Scripts/ScriptableObjects/GameVariables.cs
+++ GameJamOne/Assets/Scripts/ScriptableObjects/GameVariables.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game Variables", fileName = "GameVariables")]
